6b93ea6cb8c0fc8ccf7524a185d129165a79a341
Shohei Ogata <shohei.ogata@pepabo.com>
2017-09-26 14:18:32 +0900

    Carthageで導入するライブラリのキャッシュをする記述をしてテスト高速化を試して見た

--- piyopiyoTests/piyopiyoTests.swift
+++ piyopiyoTests/piyopiyoTests.swift
@@ -24,7 +24,6 @@ class PiyopiyoTests: XCTestCase {
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
-        XCTAssertTrue(false)
     }
     
     func testPerformanceExample() {
