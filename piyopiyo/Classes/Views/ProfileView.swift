f0a8d4c68b1e5905826124fc12692aac4ff6c888
Shohei Ogata <shohei.ogata@pepabo.com>
2017-09-27 14:51:28 +0900

    StoryBoardに載せられるように編集し、デザインを調整

--- piyopiyo/Classes/Views/ProfileView.swift
+++ piyopiyo/Classes/Views/ProfileView.swift
@@ -8,11 +8,33 @@
 
 import UIKit
 
-@IBDesignable class ProfileView: UIView {
-
+class ProfileView: UIView {
+    
     @IBOutlet weak var profileImage: UIImageView! {
         didSet {
             profileImage.layer.cornerRadius = 80
+            profileImage.layer.masksToBounds = true
         }
     }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+    
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)!
+        setup()
+    }
+    
+    fileprivate func setup() {
+        guard let view = Bundle.main.loadNibNamed("ProfileView", owner: self, options: nil)?.first as? UIView else {
+            return
+        }
+        view.frame = bounds
+        addSubview(view)
+        self.layer.borderColor = UIColor.black.cgColor
+        self.layer.borderWidth = 1
+    }
+    
 }
