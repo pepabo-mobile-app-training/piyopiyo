3d8c129d56a7d2aa80b7f35aac223cc69505402f
shizuna.ito <shizuna.ito@pepabo.com>
2017-09-27 14:49:22 +0900

    BalloonViewを複数表示するコードを追加

--- piyopiyo/Classes/Controllers/FeedViewController.swift
+++ piyopiyo/Classes/Controllers/FeedViewController.swift
@@ -12,7 +12,21 @@ class FeedViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let screenSize = UIScreen.main.bounds.size
+        let hiyokoHeight: CGFloat = 100.0
+        let balloonWidth = screenSize.width - 30 * 2
+        let balloonHeight = balloonWidth / 2
+        let balloonX = (screenSize.width - balloonWidth) / 2
+        var balloonY = screenSize.height - (balloonHeight + hiyokoHeight + 10)
+        
+        for _ in 0...4 {
+            let balloonView = BalloonView(frame: CGRect(x: balloonX, y: balloonY, width: balloonWidth, height: balloonHeight))
+            
+            view.addSubview(balloonView)
+            
+            balloonY -= 85.0
+        }
     }
 
     override func didReceiveMemoryWarning() {
