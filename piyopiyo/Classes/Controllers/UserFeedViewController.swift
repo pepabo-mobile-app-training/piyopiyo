da6f880b5d45e5ee1ef19b6890ceefee01aa67fa
Shohei Ogata <shohei.ogata@pepabo.com>
2017-09-27 17:49:42 +0900

    WebViewのコントローラを配置

---
+++ piyopiyo/Classes/Controllers/UserFeedViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  piyopiyo
+//
+//  Created by shizuna.ito on 2017/09/25.
+//  Copyright © 2017年 GMO Pepabo. All rights reserved.
+//
+
+import UIKit
+
+class UserFeedViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
+
