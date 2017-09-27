275630c850fc3eaed01143466c1a1406a01d9cca
Shohei Ogata <shohei.ogata@pepabo.com>
2017-09-27 16:53:40 +0900

    TutorialView.swiftを作成してStoryboard上に配置できることを確認した

---
+++ piyopiyo/Classes/Views/TutorialView.swift
@@ -0,0 +1,31 @@
+//
+//  TutorialView.swift
+//  piyopiyo
+//
+//  Created by shohei.ogata on 2017/09/27.
+//  Copyright © 2017年 GMO Pepabo. All rights reserved.
+//
+
+import UIKit
+
+class TutorialView: UIView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+    
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)!
+        setup()
+    }
+    
+    fileprivate func setup() {
+        guard let view = Bundle.main.loadNibNamed("TutorialView", owner: self, options: nil)?.first as? UIView else {
+            return
+        }
+        view.frame = bounds
+        addSubview(view)
+    }
+    
+}
