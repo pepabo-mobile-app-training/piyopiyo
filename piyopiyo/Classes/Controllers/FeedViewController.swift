e4d44a7beb1ecee581b603102dcf81dd281a6d30
shizuna.ito <shizuna.ito@pepabo.com>
2017-09-28 15:35:40 +0900

    BalloonViewのframeについてのプロパティを適切な名前に修正

--- piyopiyo/Classes/Controllers/FeedViewController.swift
+++ piyopiyo/Classes/Controllers/FeedViewController.swift
@@ -12,13 +12,17 @@ class FeedViewController: UIViewController {
 
     static let screenSize = UIScreen.main.bounds.size
     static let hiyokoHeight: CGFloat = 100.0
-    static let balloonWidth = screenSize.width - 30 * 2
+    
+    static let trailingMargin: CGFloat = 30.0
+    static let bottomMargin = hiyokoHeight + 10
+    
+    static let balloonWidth = screenSize.width - trailingMargin * 2
     static let balloonHeight = balloonWidth / 2
 
-    static let balloonX = 30.0 + balloonWidth
-    static let balloonY = screenSize.height - (hiyokoHeight + 10)
+    static let initialBalloonX = trailingMargin + balloonWidth
+    static let initialBalloonY = screenSize.height - bottomMargin
 
-    private let balloonView = BalloonView(frame: CGRect(x: FeedViewController.balloonX, y: FeedViewController.balloonY, width: 0, height: 0))
+    private let balloonView = BalloonView(frame: CGRect(x: FeedViewController.initialBalloonX, y: FeedViewController.initialBalloonY, width: 0, height: 0))
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,16 +33,17 @@ class FeedViewController: UIViewController {
 
     private func setBalloon() {
         UIView.animate(withDuration: 1, delay: 0.5, animations: {
-            let originBalloonY = FeedViewController.balloonY - FeedViewController.balloonHeight
+            let originBalloonX = FeedViewController.initialBalloonX - FeedViewController.balloonWidth
+            let originBalloonY = FeedViewController.initialBalloonY - FeedViewController.balloonHeight
             
-            self.balloonView.frame = CGRect(x: 30.0, y: originBalloonY, width: FeedViewController.balloonWidth, height: FeedViewController.balloonHeight)
+            self.balloonView.frame = CGRect(x: originBalloonX, y: originBalloonY, width: FeedViewController.balloonWidth, height: FeedViewController.balloonHeight)
             self.balloonView.layoutIfNeeded()
         }) { _ in
             UIView.animate(withDuration: 5, delay: 0.5, animations: {
                 self.balloonView.frame.origin.y = -FeedViewController.balloonHeight
                 self.balloonView.layoutIfNeeded()
             }) { _ in
-                self.balloonView.frame = CGRect(x: FeedViewController.balloonX, y: FeedViewController.balloonY, width: 0, height: 0)
+                self.balloonView.frame = CGRect(x: FeedViewController.initialBalloonX, y: FeedViewController.initialBalloonY, width: 0, height: 0)
                 self.balloonView.layoutIfNeeded()
                 self.setBalloon()
             }
