676112f065cdf5b80dcdcce8c2322ffbb947796b
shizuna.ito <shizuna.ito@pepabo.com>
2017-09-29 19:08:22 +0900

    Add Tutorial UITest

---
+++ piyopiyoUITests/FeedUITests.swift
@@ -0,0 +1,35 @@
+//
+//  FeedUITests.swift
+//  piyopiyoUITests
+//
+//  Created by shizuna.ito on 2017/09/29.
+//  Copyright © 2017年 GMO Pepabo. All rights reserved.
+//
+
+import XCTest
+
+class FeedUITests: XCTestCase {
+
+    override func setUp() {
+        super.setUp()
+
+        continueAfterFailure = false
+        XCUIApplication().launch()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+    func testRemoveFromFeed() {
+        let app = XCUIApplication()
+        
+        let startButton = app.buttons["startButton"]
+        
+        XCTAssert(startButton.exists)
+        
+        startButton.tap()
+        
+        XCTAssertFalse(startButton.exists)
+    }
+}
