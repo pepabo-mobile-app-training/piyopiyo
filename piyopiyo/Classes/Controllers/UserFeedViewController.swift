7f5052f97c30c2a94d0e315db6cb61032b83fed0
shizuna.ito <shizuna.ito@pepabo.com>
2017-10-10 15:43:25 +0900

    FeedTableViewCellをfeedTableViewに表示

--- piyopiyo/Classes/Controllers/UserFeedViewController.swift
+++ piyopiyo/Classes/Controllers/UserFeedViewController.swift
@@ -2,12 +2,17 @@ import UIKit
 import WebKit
 class UserFeedViewController: UIViewController {
     
-    @IBOutlet weak var avatarImageView: UIImageView!
+    @IBOutlet weak var avatarImageView: UIImageView! {
+        didSet {
+            avatarImageView.layer.cornerRadius = avatarImageView.bounds.width / 2
+            avatarImageView.layer.masksToBounds = true
+        }
+    }
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var feedTableView: UITableView! {
         didSet {
-            feedTableView.layer.cornerRadius = feedTableView.bounds.width / 2
-            feedTableView.layer.masksToBounds = true
+            let cellName = "FeedTableViewCell"
+            feedTableView.register(UINib(nibName: cellName, bundle: nil), forCellReuseIdentifier: cellName)
         }
     }
     
@@ -21,5 +26,25 @@ class UserFeedViewController: UIViewController {
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+    }
+}
+
+// MARK: - UITableViewDataSource
+
+extension UserFeedViewController: UITableViewDataSource {
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+
+        let cellName = "FeedTableViewCell"
+
+        guard let cell = feedTableView.dequeueReusableCell(withIdentifier: cellName, for: indexPath) as? FeedTableViewCell else {
+            return UITableViewCell()
+        }
+
+        return cell
     }
 }
