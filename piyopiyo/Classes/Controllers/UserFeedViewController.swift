b3e4c84b54e26bcb50588bf44c6f773a88ea24b8
shizuna.ito <shizuna.ito@pepabo.com>
2017-10-10 18:11:27 +0900

    feedTableViewCellにmicropostデータを適用

--- piyopiyo/Classes/Controllers/UserFeedViewController.swift
+++ piyopiyo/Classes/Controllers/UserFeedViewController.swift
@@ -18,11 +18,18 @@ class UserFeedViewController: UIViewController {
     }
     
     var profile: UserProfile?
+    var microposts = [Micropost]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        fetchProfile(profile: profile)
+        if let profile = profile {
+            fetchProfile(profile: profile)
+            Micropost.fetchUsersMicroposts(userID: profile.userID) { microposts in
+                self.microposts = microposts
+                self.feedTableView.reloadData()
+            }
+        }
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -33,10 +40,7 @@ class UserFeedViewController: UIViewController {
         super.didReceiveMemoryWarning()
     }
 
-    private func fetchProfile(profile: UserProfile?) {
-        guard let profile = profile else {
-            return
-        }
+    private func fetchProfile(profile: UserProfile) {
         nameLabel.text = profile.name
         avatarImageView.sd_setImage(with: profile.avatarURL, placeholderImage: UIImage(named: "avatar"))
     }
@@ -47,17 +51,18 @@ class UserFeedViewController: UIViewController {
 extension UserFeedViewController: UITableViewDataSource {
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return microposts.count
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
 
         let cellName = "FeedTableViewCell"
 
-        guard let cell = feedTableView.dequeueReusableCell(withIdentifier: cellName, for: indexPath) as? FeedTableViewCell else {
+        guard let cell = feedTableView.dequeueReusableCell(withIdentifier: cellName, for: indexPath) as? FeedTableViewCell, let profile = profile else {
             return UITableViewCell()
         }
 
+        cell.update(profile: profile, micropost: microposts[indexPath.row])
         return cell
     }
 }
