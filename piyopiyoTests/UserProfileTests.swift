cc33bdc143fe08bd2310893c807093f1daacfecf
piyoppi <3podvator@gmail.com>
2017-10-13 12:18:10 +0900

    テストのuserIDをStringに修正

--- piyopiyoTests/UserProfileTests.swift
+++ piyopiyoTests/UserProfileTests.swift
@@ -22,8 +22,8 @@ class UserProfileTests: XCTestCase {
     func testFetchUserProfile() {
         let fetchUserProfileExpectation: XCTestExpectation? = self.expectation(description: "fetchUserProfile")
 
-        MicropostUserProfile.fetchUserProfile(userID: 1) { profile in
-            XCTAssertEqual(profile.userID, 1)
+        MicropostUserProfile.fetchUserProfile(userID: "1") { profile in
+            XCTAssertEqual(profile.userID, "1")
             XCTAssertFalse(profile.name.isEmpty)
             XCTAssertNotNil(profile.avatarURL)
             fetchUserProfileExpectation?.fulfill()
