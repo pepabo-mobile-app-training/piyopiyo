1caf02726234b71f21f64a4abf9ccff7f273797b
piyoppi <3podvator@gmail.com>
2017-10-13 12:15:27 +0900

    TwitterUserProfileを追加

---
+++ piyopiyo/Classes/Models/TwitterUserProfile.swift
@@ -0,0 +1,21 @@
+//
+//  TwitterUserProfile.swift
+//  piyopiyo
+//
+//  Created by shohei.ogata on 2017/10/13.
+//  Copyright © 2017年 GMO Pepabo. All rights reserved.
+//
+
+import Foundation
+
+class TwitterUserProfile: userProfile {
+    var userID: String
+    var name: String
+    var avatarURL: URL?
+    
+    init(name: String, userID: String, avatarURL: URL?) {
+        self.name = name
+        self.userID = userID
+        self.avatarURL = avatarURL
+    }
+}
