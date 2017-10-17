ec1d89de9fd4c7d0d13561ca0d5f42cbf73da1d8
Shizuna Ito <shizuna.itoh@gmail.com>
2017-10-17 17:14:53 +0900

    Twitterの取得メソッドを追加

--- piyopiyo/Classes/Models/Tweet.swift
+++ piyopiyo/Classes/Models/Tweet.swift
@@ -7,13 +7,31 @@
 //
 
 import Foundation
+import SwifteriOS
 
 class Tweet: MicroContent {
     var userID: String
     var content: String
+    var profile: TwitterUserProfile
     
-    init(content: String, userID: String) {
+    init(content: String, userID: String, profile: TwitterUserProfile) {
         self.content = content
         self.userID = userID
+        self.profile = profile
+    }
+
+    static func fetchRandomTweets(swifter: Swifter, handler: @escaping ((Tweet) -> Void)) -> HTTPRequest {
+        return swifter.streamRandomSampleTweets(language:  ["ja"], progress: { json in
+            guard let content = json["text"].string,
+                  let userID = json["user"]["id"].double,
+                  let name = json["user"]["name"].string,
+                  let url = json["user"]["profile_image_url_https"].string else {
+                return
+            }
+            let profile = TwitterUserProfile(name: name, userID: String(userID), avatarURL: URL(string: url))
+            let tweet = Tweet(content: content, userID: String(userID), profile: profile)
+
+            handler(tweet)
+        })
     }
 }
