29ef100b5e9030c9b4f2a8451695ad90b2cc9dba
piyoppi <3podvator@gmail.com>
2017-10-18 16:57:44 +0900

    メニューのふきだしデザイン

---
+++ piyopiyo/Classes/Views/MenuView.swift
@@ -0,0 +1,13 @@
+//
+//  MenuView.swift
+//  piyopiyo
+//
+//  Created by shohei.ogata on 2017/10/18.
+//  Copyright © 2017年 GMO Pepabo. All rights reserved.
+//
+
+import UIKit
+
+class MenuView: UIView {
+
+}
