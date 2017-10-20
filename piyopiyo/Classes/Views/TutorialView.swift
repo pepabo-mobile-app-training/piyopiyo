45ee868eed64334013914be33b2665870a3e5a6e
piyoppi <3podvator@gmail.com>
2017-10-20 12:10:36 +0900

    iPhoneXで正常にチュートリアルが描画されない問題を修正

--- piyopiyo/Classes/Views/TutorialView.swift
+++ piyopiyo/Classes/Views/TutorialView.swift
@@ -35,17 +35,13 @@ class TutorialView: UIView {
         }
         view.frame = bounds
         addSubview(view)
-        
-       
-        
+        scrollView.translatesAutoresizingMaskIntoConstraints = true
         tutorialPage.translatesAutoresizingMaskIntoConstraints = true
+        
         tutorialPage.frame = CGRect(origin: CGPoint(x: 0, y: 0), size: TutorialPage.viewSize())
         
-        
         scrollView.contentSize = TutorialPage.viewSize()
-        
-        let screenSize = UIScreen.main.bounds.size
-        scrollView.bounds = CGRect(x: 0, y: 0, width: screenSize.width, height: screenSize.height)
+        scrollView.frame = CGRect(origin: CGPoint(x: 0, y: 0), size: UIScreen.main.bounds.size)
     }
     
     @IBAction func startButtonDidTap(_ sender: ColorButton) {
